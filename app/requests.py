e0493a3c506dbd35c6df75ea21f61eb266c4a4f1
Ngina <Nginagitau11@outlook.com>
2022-05-04 05:11:51 +0300

    update: adds the functions for getting the articles

--- app/requests.py
+++ app/requests.py
@@ -34,7 +34,9 @@ def get_source():
     return source_result
         
 def process_result(source_result_list):
-    '''processes sources'''
+    '''
+    Function that processes sources
+    '''
     source_result = []
     for item in source_result_list:
         name = item.get('name')
@@ -44,5 +46,65 @@ def process_result(source_result_list):
         source_result.append(source_object)
     return source_result
 
+# ARTICLES
+def get_articles(category):
+    '''
+    Function that gets the json response to our url request for articles
+    '''
+    get_url = base_url.format(category,api_key)
+    
+    with urllib.request.urlopen(get_url) as url:
+        get_data = url.read()
+        get_article_response = json.loads(get_data)
+        
+        article_result = None
+        
+        if get_article_response['articles']:
+            article_result_list = get_article_response['articles']
+            article_result = process_results(article_result_list)
+    return article_result
 
+def get_article(id):
+
+    get_article_details_url = base_url.format(id,api_key)
+    
+    with urllib.request.urlopen(get_article_details_url) as url:
+        article_details_data = url.read()
+        article_details_response = json.loads(article_details_data)
+        
+        article_object = None
+        
+        if article_details_response:
+            author = article_details_response.get('author')
+            title = article_details_response.get('title')
+            description = article_details_response.get('description')
+            url = article_details_response.get('url')
+            urlToImage = article_details_response.get('urlToImage')
+            content = article_details_response.get('content')
+            publishedAt = article_details_response.get('publishedAt')
+
+            article_object = Article(author,title,description,url,urlToImage,content,publishedAt)
+    return article_object
+
+def process_results(article_list):
+    article_result = []
+    '''
+        Function  that processes the article result and transform them to a list of Objects
+        Args:
+            article_list: A list of dictionaries that contain aricle details
+        Returns :
+            article_results: A list of article objects
+    '''
+    for item in article_list:
+        author = item.get('author')
+        title = item.get('title')
+        description = item.get('description')
+        url = item.get('url')
+        urlToImage = item.get('urlToImage')
+        content = item.get('content')
+        publishedAt = item.get('publishedAt')
+        
+        article_object = Article(author,title,description,url,urlToImage,content,publishedAt)
+        article_result.append(article_object)
+    return article_result
 
