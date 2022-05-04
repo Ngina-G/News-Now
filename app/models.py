44dd5a54ecd350e886b29bae84df1069b8d1c9f8
Ngina <Nginagitau11@outlook.com>
2022-05-04 04:18:29 +0300

    udate:adds models

---
+++ app/models.py
@@ -0,0 +1,17 @@
+class Article:
+    
+    def __init__(self,author,title,description,url,urlToImage,content,publishedAt):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.content = content
+        self.content = publishedAt
+
+class Source:
+    source = []
+    
+    def __init__(self,name,url):
+        self.name = name
+        self.url = url
